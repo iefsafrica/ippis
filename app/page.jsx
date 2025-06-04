586d190b07bd06859e5c5d797cd6f13978d2e6ff
Lace-daiki <90939296+Lace-daiki@users.noreply.github.com>
2025-06-04 18:22:52 +0100

    Update

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import VerificationForm from "@/components/verification-form";
 
 export default function Home() {
   return (
-    <main className="w-[1000px] min-h-screen bg-pink-50 py-10 px-24">
+    <main className="min-h-screen bg-pink-50 py-10 px-24">
       <div className="w-full max-w-3xl mx-auto bg-white p-24 rounded-lg shadow-md">
         <Stepper current={1} />
         <VerificationForm />
